dff919cb77a93b53c13f5acee0c5183dcdd76bfb
Ignacio Horcada bernal <ihorcada@c2r4s3.42madrid.com>
2020-02-07 18:12:08 +0100

    added libraries

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,19 @@
 /*   By: ihorcada <ihorcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 14:19:38 by ihorcada          #+#    #+#             */
-/*   Updated: 2020/02/07 14:21:05 by ihorcada         ###   ########.fr       */
+/*   Updated: 2020/02/07 18:09:37 by ihorcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int	get_next_line(int fd, char **line)
 {
-	
+	int fd;
+
+	fd = open 
 }
