17f379126945f061ff2779dbf59624cfb81e7892
Ignacio Horcada bernal <ihorcada@c2r4s3.42madrid.com>
2020-02-10 18:06:59 +0100

    OPEN, READ

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,28 @@
 /*   By: ihorcada <ihorcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 14:19:38 by ihorcada          #+#    #+#             */
-/*   Updated: 2020/02/07 18:09:37 by ihorcada         ###   ########.fr       */
+/*   Updated: 2020/02/10 17:35:07 by ihorcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include "get_next_line.h"
 
 int	get_next_line(int fd, char **line)
 {
-	int fd;
+	int		ret;
+	char	*buffer;
 
-	fd = open 
+	line = NULL;
+	buffer = 0;
+	if (!(buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE)))
+		return (-1);
+	if (!(fd = open("test.txt", O_RDONLY)))
+		return (-1);
+	if (!(ret = read(fd, buffer, BUFFER_SIZE)))
+		return (-1);
+	buffer[ret] = '\0';
+
+	ft_putnbr_fd(ret, fd);
+	ft_putstr_fd(buffer, fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,26 @@
 /*   By: ihorcada <ihorcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 14:25:26 by ihorcada          #+#    #+#             */
-/*   Updated: 2020/02/07 14:25:27 by ihorcada         ###   ########.fr       */
+/*   Updated: 2020/02/10 15:53:38 by ihorcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <string.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+#endif
+
+int		get_next_line(int fd, char **line);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+
+#endif
