b124661a35c9e387b49a82a5f447af3b2075ddc5
Ignacio Horcada bernal <ihorcada@c2r4s3.42madrid.com>
2020-02-14 18:50:16 +0100

    adding strjoin

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ihorcada <ihorcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 14:19:38 by ihorcada          #+#    #+#             */
-/*   Updated: 2020/02/14 17:57:36 by ihorcada         ###   ########.fr       */
+/*   Updated: 2020/02/14 18:49:36 by ihorcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	get_next_line(int fd, char **line)
 	char	*buf;
 	int bytes_read;
 	static char *buf_copy[4096];
-	char *temp_str;
+	char *temp;
 	
 	if (*line == NULL || fd < 0 || BUFFER_SIZE <= 0 ||
 	 (!(buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1))))
@@ -28,9 +28,9 @@ int	get_next_line(int fd, char **line)
 			buf[bytes_read] = '\0';
 			if (!buf_copy[fd])
 				return(-1);
-			
+			while(!(newline_check(buf)))
+				buf_copy = ft_strjoin(buf_copy, buf);
 		}
-
 	*line = ft_substr(buf_copy, 0, strlen(buf_copy) - strlen(newline_check));
 	return (0);
 }
